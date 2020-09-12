971be7bc276ced02eb2fc4d22ebfbe644ee59cf2
12parkhy-test <12parkhy@kakao.com>
2020-09-12 22:19:35 +0900

    Updates

---
+++ precache-manifest.709755b5187be194fa77dbd4f14cc0f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ce0162a0df8f6827ae3e19cd604824",
+    "url": "/test-2/index.html"
+  },
+  {
+    "revision": "12e261ad7893d5fafbfa",
+    "url": "/test-2/static/css/main.6b0ccb04.chunk.css"
+  },
+  {
+    "revision": "efbce2b364524b05486c",
+    "url": "/test-2/static/js/2.29ea150d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-2/static/js/2.29ea150d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e261ad7893d5fafbfa",
+    "url": "/test-2/static/js/main.b1d8e696.chunk.js"
+  },
+  {
+    "revision": "49b8c5ffd28f06fd2b94",
+    "url": "/test-2/static/js/runtime-main.73bf72fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
